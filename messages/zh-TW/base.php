e9abafe819e93c2238c7f9e44cd69ee5f202cee1
thirsight <thirsight@gmail.com>
2015-10-28 16:35:33 +0800

    dev

--- messages/zh-TW/base.php
+++ messages/zh-TW/base.php
@@ -1,6 +1,10 @@
 <?php
 
 return [
+    'Empty data.' => '數據不能為空。',
+    'Invalid data.' => '無效的數據。',
+    'Data validate failed.' => '數據驗證失敗。',
+
     'Invalid phone.' => '無效的行動電話。',
     'Invalid chinese.' => '無效的中文字符。',
     'Invalid alipay account.' => '無效的支付寶賬號。',
